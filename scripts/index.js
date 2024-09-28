ff2224d702d3c6cde35023c3409a4b4d74967eba
jiixxor <jorsixjixxor@gmail.com>
2024-09-28 07:08:43 +0700

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Sep 28, 2024',
+  name: 'PITCHA MY BRO'
 };
 
 function hideEverything() {
